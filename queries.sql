29fbe79c61bb6b13b50ca12524ee799ca7e67b60
Julian Montague <juliangm@stud.ntnu.no>
2022-11-18 11:41:18 +0100

    fix: change first query question to find plan B
    Previously, this query found employees who worked on both project "A"
    and project "B". However, after re-reading the question I realized that
    it was asking about plan "B".

--- queries.sql
+++ queries.sql
@@ -1,12 +1,3 @@
 -- How many employees of for a project titled "A" are involved in its plan “B”?
-SELECT COUNT(*) FROM EmployeeActivity WHERE employeeid IN
-    (SELECT employeeid FROM EmployeeActivity WHERE activityid IN
-        (SELECT activityid FROM Activity WHERE planid IN
-            (SELECT planid FROM Plan WHERE projectname = "A")
-        )
-    )
-AND
-activityid IN
-    (SELECT activityid FROM Activity WHERE planid IN
-        (SELECT planid FROM Plan WHERE projectname = "B")
-    )
+SELECT COUNT(*) FROM EmployeeActivity WHERE activityid IN
+    (SELECT activityid FROM Activity WHERE planname = "B")
