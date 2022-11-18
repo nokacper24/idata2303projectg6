92e22781241f801fb0ba185da8f370d74e16f578
Julian Montague <juliangm@stud.ntnu.no>
2022-11-18 11:43:05 +0100

    feat: add the query questions without solutions

--- queries.sql
+++ queries.sql
@@ -1,3 +1,12 @@
 -- How many employees of for a project titled "A" are involved in its plan “B”?
+<<<<<<< Updated upstream
 SELECT COUNT(*) FROM EmployeeActivity WHERE activityid IN
     (SELECT activityid FROM Activity WHERE planname = "B")
+
+-- Retrieve the names of plans made for project “A” with least cost.
+
+-- For each employee retrieve the name, project name and plan name with the most working time.
+
+-- Retrieve all the employee’s name and their least working time with respect to different project.
+
+-- Retrieve all the plans for project with order of their working period.
